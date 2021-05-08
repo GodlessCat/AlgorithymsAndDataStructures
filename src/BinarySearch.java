958efc9acb4af65f2db527e3cef86e8599194de2
Ilya Tsvyk <Godless.catt@gmail.com>
2021-05-08 13:35:53 +0300

    Adding binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class BinarySearch {
+
+    public static int binarySearch(List<Integer> array, int num) {
+        int leftIndex = 0;
+        int rightIndex = array.size() - 1;
+
+        while (leftIndex <= rightIndex) {
+            int currentIndex = (leftIndex + rightIndex) / 2;
+
+            if (array.get(currentIndex) == num){
+                return currentIndex;
+            }
+
+            if (array.get(currentIndex) < num) {
+                leftIndex = currentIndex + 1;
+            } else {
+                rightIndex = currentIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Binary Search
+        List<Integer> binarySearchArray = Arrays.asList(1, 2, 3, 4, 5);
+        int intToFind = 4;
+        System.out.println("Binary search: " + intToFind + " is on " + BinarySearch.binarySearch(binarySearchArray, intToFind) + " position");
     }
 }
